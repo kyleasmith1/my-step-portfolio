11bc2a64df33283203fadcc0d21e19112c0444fb
Kyle Smith <kysmith152@gmail.com>
2020-07-16 20:03:25 +0000

    Find a meeting implementation complete

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,71 @@
 
 package com.google.sps;
 
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public final class FindMeetingQuery {
+
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+    Collection<String> requestAttendees= request.getAttendees();
+    Collection<String> optionalAttendees = request.getOptionalAttendees();
+    long requestDuration = request.getDuration();
+
+    // Base Case(s)
+    if (requestDuration > 1440) {
+        return Arrays.asList(); 
+    }
+
+    // General Case(s)
+    int startTemp = 0;
+    int endTemp = 0;
+    long durationTemp = 0;
+    int similarities = 0;
+
+    ArrayList<TimeRange> queryCollection = new ArrayList<TimeRange>();
+
+    for(Event event: events) {
+        int eventStart = event.getWhen().start();
+        int eventEnd = event.getWhen().end();
+
+        // At least one similar attendee
+        if (!Collections.disjoint(requestAttendees, event.getAttendees())) {
+            similarities += 1;
+
+            // Checks for overlap
+            if (eventStart <= timeStart && eventEnd > timeStart) {
+                timeStart = eventEnd;
+            }
+
+            endTemp = eventStart;
+            if (endTemp > timeStart) {
+                durationTemp = endTemp - timeStart;
+
+                if (requestDuration <= durationTemp) {
+                    queryCollection.add(TimeRange.fromStartEnd(timeStart, endTemp, false));
+                }
+
+                timeStart = eventEnd;
+            }    
+        }
+    }
+
+    if (similarities == 0) {
+        return Arrays.asList(TimeRange.WHOLE_DAY);
+    }
+
+    if (timeStart != 1440) {
+        queryCollection.add(TimeRange.fromStartEnd(timeStart, 1440, false));
+    }
+
+    return queryCollection;
+  } 
 }
+
+
+
+
+
+
