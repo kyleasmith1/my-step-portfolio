90bb163ca4101584228fa654708457879b4a24c8
Kyle Smith <kysmith152@gmail.com>
2020-07-16 20:10:54 +0000

    Update the find a meeting implementation

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -22,19 +22,15 @@ import java.util.Collections;
 public final class FindMeetingQuery {
 
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    Collection<String> requestAttendees= request.getAttendees();
-    Collection<String> optionalAttendees = request.getOptionalAttendees();
-    long requestDuration = request.getDuration();
-
+      
     // Base Case(s)
-    if (requestDuration > 1440) {
+    if (request.getDuration() > 1440) {
         return Arrays.asList(); 
     }
 
     // General Case(s)
     int startTemp = 0;
     int endTemp = 0;
-    long durationTemp = 0;
     int similarities = 0;
 
     ArrayList<TimeRange> queryCollection = new ArrayList<TimeRange>();
@@ -44,23 +40,23 @@ public final class FindMeetingQuery {
         int eventEnd = event.getWhen().end();
 
         // At least one similar attendee
-        if (!Collections.disjoint(requestAttendees, event.getAttendees())) {
+        if (!Collections.disjoint(request.getAttendees(), event.getAttendees())) {
             similarities += 1;
 
             // Checks for overlap
-            if (eventStart <= timeStart && eventEnd > timeStart) {
-                timeStart = eventEnd;
+            if (eventStart <= startTemp && eventEnd > startTemp) {
+                startTemp = eventEnd;
             }
 
             endTemp = eventStart;
-            if (endTemp > timeStart) {
-                durationTemp = endTemp - timeStart;
+            if (endTemp > startTemp) {
+                long durationTemp = endTemp - startTemp;
 
-                if (requestDuration <= durationTemp) {
-                    queryCollection.add(TimeRange.fromStartEnd(timeStart, endTemp, false));
+                if (request.getDuration() <= durationTemp) {
+                    queryCollection.add(TimeRange.fromStartEnd(startTemp, endTemp, false));
                 }
 
-                timeStart = eventEnd;
+                startTemp = eventEnd;
             }    
         }
     }
@@ -69,16 +65,10 @@ public final class FindMeetingQuery {
         return Arrays.asList(TimeRange.WHOLE_DAY);
     }
 
-    if (timeStart != 1440) {
-        queryCollection.add(TimeRange.fromStartEnd(timeStart, 1440, false));
+    if (startTemp != 1440) {
+        queryCollection.add(TimeRange.fromStartEnd(startTemp, 1440, false));
     }
 
     return queryCollection;
   } 
 }
-
-
-
-
-
-
